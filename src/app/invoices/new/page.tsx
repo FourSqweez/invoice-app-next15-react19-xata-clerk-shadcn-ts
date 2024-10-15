a6a207ece9bba743743101d02318467515264a05
Foursqweez <jeerasak.code@gmail.com>
2024-10-15 12:29:30 +0700

    feat: Create invoice page

---
+++ src/app/invoices/new/page.tsx
@@ -0,0 +1,11 @@
+const InvoicePage = () => {
+  return (
+    <main className="mx-auto my-12 flex h-full max-w-5xl flex-col justify-center gap-6 text-center">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold">Invoices</h1>
+      </div>
+    </main>
+  );
+};
+
+export default InvoicePage;
