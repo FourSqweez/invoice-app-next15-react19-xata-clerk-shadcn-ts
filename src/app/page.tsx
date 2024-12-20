cf5f3c2b3564c222c94c26a524705ad776bfde68
Foursqweez <jeerasak.code@gmail.com>
2024-12-20 15:41:40 +0700

    feat: Create table schemas for Invoice data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { db } from "@/db";
+import { sql } from "drizzle-orm";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const result = await db.execute(sql`SELECT current_database()`);
   return (
     <main className="mx-auto flex h-screen max-w-5xl flex-col justify-center gap-6 text-center">
+      {JSON.stringify(result)}
       <h1 className="text-5xl font-bold">Invoicipedia</h1>
       <p>
         <Button asChild>
